86a926140b7ecc7e31844fe4067bf32ae76a4786
m22belau <m22belau@gmail.com>
2025-04-22 23:41:05 -1000

    Added About Us page and updated navbar

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/<name>', methods=['GET'])
-def hello_name(name):
-    return render_template('index.html', name=name)
+@app.route('/', methods=['GET'])
+def home_page():
+    return render_template('index.html')
 
 ### For reference, this is the /<name> route without render_template
 # @app.route('/<name>', methods=['GET'])
